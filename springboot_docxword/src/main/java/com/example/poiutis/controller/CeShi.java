50890dc5fb4db6be5946bc5efc64c0f42a94053a
tom_glb <goolMorning_glb@webcode.com>
2019-07-17 14:46:04 +0800

    测试获取resource下路径名

--- springboot_docxword/src/main/java/com/example/poiutis/controller/CeShi.java
+++ springboot_docxword/src/main/java/com/example/poiutis/controller/CeShi.java
@@ -2,14 +2,18 @@ package com.example.poiutis.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.file.Path;
 
 /**
- * @ClassName CeShi
+ * @ClassName  获取resource下的文件夹路径名地址
  * @Description TODO
  * @Author wushaopei
  * @Date 2019/7/16 0:04
@@ -22,8 +26,14 @@ public class CeShi {
 
     @RequestMapping(value = "/poidaochu/getDataByWord", method = RequestMethod.GET)
     public Object getOpenInvoicePage(HttpServletRequest request){
-
-        return "hello";
+        Resource resource = new ClassPathResource("template/1.txt");
+        String Path = null;
+        try {
+             Path = resource.getFile().getPath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Path;
     }
 
 
