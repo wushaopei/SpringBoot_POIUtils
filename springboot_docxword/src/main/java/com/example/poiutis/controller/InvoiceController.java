f3af503d283ed5a2106f56cbb7bf12d23d6ebbba
tom_glb <goolMorning_glb@webcode.com>
2019-07-17 21:04:00 +0800

    修改接口路径

--- springboot_docxword/src/main/java/com/example/poiutis/controller/InvoiceController.java
+++ springboot_docxword/src/main/java/com/example/poiutis/controller/InvoiceController.java
@@ -28,7 +28,7 @@ import java.util.*;
  *
  */
 @RestController
-@RequestMapping(value = "/aiicp")
+@RequestMapping(value = "/webcode")
 public class InvoiceController {
 
     private static Logger logger = LoggerFactory.getLogger(InvoiceController.class);
